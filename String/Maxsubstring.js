ff232924f4e420acc333db14475ac522bde452cb
Anagha-2023 <140034415+Anagha-2023@users.noreply.github.com>
2024-03-13 16:15:35 +0530

    allSubstrings

---
+++ String/Maxsubstring.js
@@ -0,0 +1,22 @@
+function findAllSubstrings(input){
+  const substrings = [];
+
+  function generateSubstrings(start,current){
+    if(start === input.length){
+      substrings.push(current);
+      return;
+    }
+
+    generateSubstrings(start+1,current+input[start]);
+
+    generateSubstrings(start+1,current);
+
+
+  }
+  generateSubstrings(0,'');
+  return substrings;
+}
+
+const input =  'ABCD';
+const allsubstrings = findAllSubstrings(input);
+console.log(allsubstrings);
