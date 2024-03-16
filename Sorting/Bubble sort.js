8d48d8a114d1f4b154e4dd0b9e702d4166873ca3
Anagha-2023 <140034415+Anagha-2023@users.noreply.github.com>
2024-03-16 20:10:37 +0530

    Bubble,Insertion

---
+++ Sorting/Bubble sort.js
@@ -0,0 +1,17 @@
+const arr = [2,5,7,9,3,1,8];
+
+function BubbleSort(arr){
+  for(let i=0;i<arr.length-1;i++){
+    for(let j=0;j<arr.length-i-1;j++){
+      if(arr[j]>arr[j+1]){
+        // [arr[j],arr[j+1]] = [arr[j+1],arr[j]]
+        const temp = arr[j];
+        arr[j] = arr[j+1];
+        arr[j+1] = temp;
+      }
+    }
+  }
+  
+  return arr;
+}
+console.log(BubbleSort(arr));
