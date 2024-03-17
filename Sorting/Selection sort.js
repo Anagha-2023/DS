e829b59b7ac2466eaf79a6df9c4bf0ef3bbb6742
Anagha-2023 <140034415+Anagha-2023@users.noreply.github.com>
2024-03-17 18:36:16 +0530

    selection sort

---
+++ Sorting/Selection sort.js
@@ -0,0 +1,19 @@
+const arr = [2,3,1,4,8,5];
+
+function Selectionsort(arr){
+  for(let i=0;i<arr.length;i++){
+    let small = i;
+    for(let j=i+1;j<arr.length;j++){
+      if(arr[small]>arr[j]){
+        small = j;
+      }
+    }
+
+    let temp = arr[i];
+    arr[i] = arr[small];
+    arr[small] = temp;
+  }
+  return arr;
+}
+
+console.log(Selectionsort(arr));
--- practice.js
+++ practice.js
@@ -1,20 +1,33 @@
-function findAllSubstrings(str){
-  let substrings = [];
+// const arr = [3,6,9,3,7,4,1,0];
 
-  function generateSubstrings(start,current){
-    if(start === str.length){
-      substrings.push(current);
-      return;
+// function Bubbleseort(arr){
+//   for(let i=0;i<arr.length-1;i++){
+//     for(let j=0;j<arr.length;j++){
+//       if(arr[j]>arr[j+1]){
+//         [arr[j],arr[j+1]] = [arr[j+1],arr[j]]
+//       }
+//     }
+//   }
+//   return arr;
+// }
+
+// console.log(Bubbleseort(arr));
+
+const arr = [3,6,9,3,7,4,1,0];
+
+function InsertionSort(arr){
+  for(let i=0;i<arr.length;i++){
+    let current = arr[i];
+    let j = i-1;
+
+    while(j>=0 && arr[j] > current){
+      arr[j+1] = arr[j];
+      j--;
     }
-
-    generateSubstrings(start+1,current+str[start]);
-    generateSubstrings(start+1,current)
+    arr[j+1] = current;
   }
 
-  generateSubstrings(0,'');
-  return substrings;
+  return arr;
 }
 
-const str = 'AZC';
-let allsubstrings = findAllSubstrings(str);
-console.log(allsubstrings);
+console.log(InsertionSort(arr));
