2d995c23a8c0d5e4477699f81f848be041462656
Anagha-2023 <140034415+Anagha-2023@users.noreply.github.com>
2024-03-18 19:59:33 +0530

    Quick sort

---
+++ Sorting/Quick sort.js
@@ -0,0 +1,43 @@
+function QuickSort(arr,startIndx=0,endIndex=arr.length-1){
+  quicksortHelper(arr, startIndx, endIndex);
+  return arr;
+}
+
+function quicksortHelper(arr,startIndx,endIndex){
+  if(startIndx>=endIndex){
+    return;
+  }
+
+  let PivotIndx = startIndx;
+  let leftIndx = startIndx+1;
+  let rightIndx = endIndex;
+
+  while (leftIndx<=rightIndx) {
+    if(arr[leftIndx]>arr[PivotIndx] && arr[rightIndx]<arr[PivotIndx]){
+      swap(arr,leftIndx,rightIndx);
+      leftIndx++;
+      rightIndx--;
+    }
+    if(arr[leftIndx]<=arr[PivotIndx]){
+      leftIndx++;
+    }
+    if(arr[rightIndx]>=arr[PivotIndx]){
+      rightIndx--;
+    }
+  }
+
+  swap(arr,rightIndx,PivotIndx);
+  quicksortHelper(arr,startIndx,rightIndx-1);
+  quicksortHelper(arr,rightIndx+1,endIndex);
+
+}
+
+function swap(arr,i,j){
+  let temp = arr[i];
+  arr[i] = arr[j];
+  arr[j] = temp;
+}
+
+const arr = [3,4,61,2,9,4,3];
+let result = QuickSort(arr);
+console.log(result);
--- practice.js
+++ practice.js
@@ -13,21 +13,41 @@
 
 // console.log(Bubbleseort(arr));
 
-const arr = [3,6,9,3,7,4,1,0];
+// const arr = [3,6,9,3,7,4,1,0];
 
-function InsertionSort(arr){
+// function InsertionSort(arr){
+//   for(let i=0;i<arr.length;i++){
+//     let current = arr[i];
+//     let j = i-1;
+
+//     while(j>=0 && arr[j] > current){
+//       arr[j+1] = arr[j];
+//       j--;
+//     }
+//     arr[j+1] = current;
+//   }
+
+//   return arr;
+// }
+
+// console.log(InsertionSort(arr));
+
+
+const arr = [2,3,1,8,5];
+
+function Selectionsort(arr){
   for(let i=0;i<arr.length;i++){
-    let current = arr[i];
-    let j = i-1;
-
-    while(j>=0 && arr[j] > current){
-      arr[j+1] = arr[j];
-      j--;
+    let small = i;
+    for(let j=i+1;j<arr.length;j++){
+      if(arr[small]>arr[j]){
+        small  = j;
+      }
     }
-    arr[j+1] = current;
+    let temp = arr[i];
+    arr[i] = arr[small];
+    arr[small] = temp;
   }
-
   return arr;
 }
 
-console.log(InsertionSort(arr));
+console.log(Selectionsort(arr));
